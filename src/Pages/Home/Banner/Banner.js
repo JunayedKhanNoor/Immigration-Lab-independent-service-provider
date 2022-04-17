d6206fd112fd29cf1711ebc8d2bc30035ca2839f
163002029.junayed <junayednoor@gmail.com>
2022-04-17 09:16:17 +0600

    Banner Added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import banner1 from "../../../Images/Banner/banner1.jpg";
+import banner2 from "../../../Images/Banner/banner2.jpg";
+import banner3 from "../../../Images/Banner/banner3.jpg";
 
 const Banner = () => {
-    return (
-        <div>
-            <h1>This is Banner</h1>
-        </div>
-    );
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 img-fluid"
+          src={banner1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3 className="shadow fw-bold">Citizenship by Investment</h3>
+          <p>
+            Invest in the Second Passport, Fast Citizenship in a few months
+            Caribbean Passport and more.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 img-fluid"
+          src={banner2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3 className="shadow fw-bold">Immigration Consultants</h3>
+          <p>Providing Immigration Services In Canada.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 img-fluid"
+          src={banner3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3 className="shadow fw-bold">International Experience</h3>
+          <p>
+          We love what we do, and we are looking forward to serving you.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default Banner;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,7 +5,7 @@ import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container className="text-uppercase">
         <Navbar.Brand
           as={Link}
@@ -18,10 +18,10 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold"><CustomLink to='/success-stories'>Success Stories</CustomLink></Nav.Link>
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold"><CustomLink to='/blogs'>Blogs</CustomLink></Nav.Link>
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold"><CustomLink to='/about'>About</CustomLink></Nav.Link>
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold"><CustomLink to='/login'>Login</CustomLink></Nav.Link>
+            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/success-stories'>Success Stories</CustomLink></Nav.Link>
+            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/blogs'>Blogs</CustomLink></Nav.Link>
+            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/about'>About</CustomLink></Nav.Link>
+            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/login'>Login</CustomLink></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
