14468a329d5ca32581df7b4724808e509d5bc659
163002029.junayed <junayednoor@gmail.com>
2022-04-18 09:21:52 +0600

    Not Found Page

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import error from "../../../Images/Banner/error.jpg"
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found</h1>
+        <div  className="d-flex justify-content-center">
+            <img style={{maxHeight:'900px'}} className='img-fluid' src={error} alt="" />
         </div>
     );
 };
