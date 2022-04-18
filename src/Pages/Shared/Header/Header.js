2fada1836ae1846805c8e78f7690261d6e3d7ddf
163002029.junayed <junayednoor@gmail.com>
2022-04-18 14:49:08 +0600

    Code cleaning

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -25,16 +25,6 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-           {/*  <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/home'>Services</CustomLink></Nav.Link>
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/success-stories'>Success Stories</CustomLink></Nav.Link>
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/blogs'>Blogs</CustomLink></Nav.Link>
-            <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/about'>About</CustomLink></Nav.Link>
-            {
-              user?
-              <Nav.Link onClick={logOut} as={Link} to='/' className="text-light fs-6 fw-bold mx-1 ">Sign Out</Nav.Link>
-              :
-              <Nav.Link  style={{ letterSpacing: "1px" }} className="fs-6 fw-bold mx-1"><CustomLink to='/login'>Login</CustomLink></Nav.Link>
-            } */}
             <CustomLink className="text-light fs-6 fw-bold mx-2 "  to='/home'>Services</CustomLink>
             <CustomLink className="text-light fs-6 fw-bold mx-2 " to='/success-stories'>Success Stories</CustomLink>
             <CustomLink className="text-light fs-6 fw-bold mx-2 " to='/blogs'>Blogs</CustomLink>
